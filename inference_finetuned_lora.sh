19c12daf44046edbf0de21c6e229bce62fd04b18
TheRookie2021 <csrookie2022@gmail.com>
2024-12-23 20:14:44 +0800

    test for 3080

--- inference_finetuned_lora.sh
+++ inference_finetuned_lora.sh
@@ -1,3 +1,3 @@
-python inference/inference.py --model finetune_llava-1.5-7b-hf_lora_5_weight_decay/checkpoint-36010 --save_json_path inference/submission_5_finetuned_lora_5_36010_r16_no_shuffle.json --batch_size 16 && /
-python inference/inference.py --model finetune_llava-1.5-7b-hf_lora_3_2/checkpoint-28810 --save_json_path inference/submission_3_finetuned_lora_3_2_no_shuffle.json --batch_size 16 && /
-python inference/inference.py --model finetune_llava-1.5-7b-hf_lora_3_3/checkpoint-28810 --save_json_path inference/submission_4_finetuned_lora_3_3_no_shuffle.json --batch_size 16
+# python inference/inference.py --model finetune_llava-1.5-7b-hf_lora_6_weight_decay_continue/checkpoint-7200 --save_json_path inference/submission_7_finetuned_lora_5_resume_r16_maxT300.json --batch_size 16
+# python inference/inference.py --model finetune_llava-1.5-7b-hf_lora_3_2/checkpoint-28810 --save_json_path inference/submission_3_finetuned_lora_3_2_no_shuffle.json --batch_size 16 && /
+python inference/inference.py --model finetune_llava-1.5-7b-hf_lora_3_3/checkpoint-28810 --save_json_path inference/test.json --batch_size 16
